7016572f1281bbb5f0a3f9d096667a10354a1448
AnyCreation <gson7990@gmail.com>
2024-11-02 09:14:34 +1100

    version 0.1
    можеть выполнять простие уровнения

--- Equation.py
+++ Equation.py
@@ -1,34 +1,47 @@
 class Solution:
     def __init__(self, equation: list[str]):
         self.equation = equation
-        self.sym = {}
-        self.Found()
-
-    def Found(self):
-        self.sym.clear()
-        S = ["+", "-", "*", "/", "^"]
-        for C in range(len(self.equation)):
-            if self.equation[C] in S:
-                self.sym[C] = self.equation[C]
-
-    def Act(self):
-        act = {
+        
+        self.acts = {
             "+": lambda a, b: a + b,
             "-": lambda a, b: a - b,
             "*": lambda a, b: a * b,
             "/": lambda a, b: a / b,
             "^": lambda a, b: a ** b,
         }
+
+    def SpaceDelete(self):
+        for Delete in range(len(self.equation)):
+            if self.equation[-1] == 'SPACE':
+                self.equation.pop()
+
+
+    def Act(self):
+        Repeat = 0
+        for FoundActs in self.equation:
+            if FoundActs in self.acts.keys():
+                Repeat += 1
+
+        for _ in range(Repeat): # Повторения равно количество математических символов
+            for ActIndex in range(len(self.equation)):
+                if self.equation[ActIndex] in self.acts.keys():
+
+                    # решение и вставление
+                    res = self.acts[self.equation[ActIndex]](float(self.equation[ActIndex - 1]), float(self.equation[ActIndex + 1]))
+                    self.equation[ActIndex] = res # заменять математический символ на ответ уравнений
+
+                    # Чистка
+                    self.equation.pop(ActIndex + 1) # удаляет число после МС
+                    self.equation.append("SPACE") # кастил для избавления от "IndexError: list index out of range"
+
+                    self.equation.pop(ActIndex - 1) # удаляет число перед МС
+                    self.equation.append("SPACE") # кастил для избавления от "IndexError: list index out of range"
+
+        self.SpaceDelete()
+
         
-        self.Found()
-        for i in self.sym.keys():
-            print(self.equation[i])
-            Result = act[self.equation[i]](float(self.equation[i - 1]), float(self.equation[i + 1]))
-            self.equation[i] = Result
-            self.equation.pop(i + 1)
-            self.equation.pop(i - 1)
-        return self.equation
 
 if __name__ == "__main__":
-    S = Solution(["2", "+", "12", "-", "5"])
-    print(S.Act())
+    S = Solution(["5", "/", "3"])
+    S.Act()
+    print(S.equation)
