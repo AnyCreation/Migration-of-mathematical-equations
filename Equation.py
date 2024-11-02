b593a0c5f3ff0826a8990ed5967864d94b9c56e8
AnyCreation <gson7990@gmail.com>
2024-11-03 03:56:18 +1100

    solution preference and optimization

--- Equation.py
+++ Equation.py
@@ -1,15 +1,27 @@
 class Solution:
     def __init__(self, equation: list[str]):
         self.__equation = equation
-        
-        self.__acts = {
+
+        self.__mathSymbols = ("+", "-", "*", "/", "^")
+        self.__MINacts = {
             "+": lambda a, b: a + b,
             "-": lambda a, b: a - b,
+        }
+        self.__MIDacts = {
             "*": lambda a, b: a * b,
             "/": lambda a, b: a / b,
+        }
+        self.__MAXacts = {
             "^": lambda a, b: a ** b,
         }
 
+    def SpaceCreate(self, Index):
+        self.__equation.pop(Index + 1) # удаляет число после МС
+        self.__equation.append("SPACE") # кастил для избавления от "IndexError: list index out of range"
+
+        self.__equation.pop(Index - 1) # удаляет число перед МС
+        self.__equation.append("SPACE") # кастил для избавления от "IndexError: list index out of range"
+        
     def SpaceDelete(self): # удаляет костыли
         while self.__equation and self.__equation[-1] == "SPACE": # while проверяет если список не пустой and проверяет если последний элемент равен "SPACE"
             self.__equation.pop()
@@ -18,23 +30,43 @@ class Solution:
     def Act(self):
         Repeat = 0
         for FoundActs in self.__equation:
-            if FoundActs in self.__acts.keys():
+            if FoundActs in self.__mathSymbols:
                 Repeat += 1
 
+        lenEquation = range(len(self.__equation))
+        # сперва решаеть степень
         for _ in range(Repeat): # Повторения равно количество математических символов
-            for ActIndex in range(len(self.__equation)):
-                if self.__equation[ActIndex] in self.__acts.keys():
-
+            for ActIndex in lenEquation:
+                # ^
+                if self.__equation[ActIndex] in self.__MAXacts.keys():
                     # решение и вставление
-                    res = self.__acts[self.__equation[ActIndex]](float(self.__equation[ActIndex - 1]), float(self.__equation[ActIndex + 1]))
+                    res = self.__MAXacts[self.__equation[ActIndex]](float(self.__equation[ActIndex - 1]), float(self.__equation[ActIndex + 1]))
                     self.__equation[ActIndex] = res # заменять математический символ на ответ уравнений
 
-                    # Чистка
-                    self.__equation.pop(ActIndex + 1) # удаляет число после МС
-                    self.__equation.append("SPACE") # кастил для избавления от "IndexError: list index out of range"
+                    self.SpaceCreate(ActIndex)
 
-                    self.__equation.pop(ActIndex - 1) # удаляет число перед МС
-                    self.__equation.append("SPACE") # кастил для избавления от "IndexError: list index out of range"
+        # потом решаеть умножения и деления
+        for _ in range(Repeat): # Повторения равно количество математических символов
+            for ActIndex in lenEquation:
+                # *; /
+                if self.__equation[ActIndex] in self.__MIDacts.keys():
+                    # решение и вставление
+                    res = self.__MIDacts[self.__equation[ActIndex]](float(self.__equation[ActIndex - 1]), float(self.__equation[ActIndex + 1]))
+                    self.__equation[ActIndex] = res # заменять математический символ на ответ уравнений
+
+                    self.SpaceCreate(ActIndex)
+
+        # напоследок решает плюс и минус
+        for _ in range(Repeat): # Повторения равно количество математических символов
+            for ActIndex in lenEquation:
+                # +; -
+                if self.__equation[ActIndex] in self.__MINacts.keys():
+
+                    # решение и вставление
+                    res = self.__MINacts[self.__equation[ActIndex]](float(self.__equation[ActIndex - 1]), float(self.__equation[ActIndex + 1]))
+                    self.__equation[ActIndex] = res # заменять математический символ на ответ уравнений
+
+                    self.SpaceCreate(ActIndex)
 
         self.SpaceDelete()
 
@@ -47,5 +79,5 @@ class Solution:
         
 
 if __name__ == "__main__":
-    S = Solution(["5", "/", "3"])
+    S = Solution(["3", "^", "4", "*", "3"])
     print(S.solution())
